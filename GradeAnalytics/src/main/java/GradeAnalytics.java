08240d7d1e1a59bd3c85f10e9e02c6d3a3734ec1
kasteel4 <kasteel4@asu.edu>
2019-12-03 15:49:07 -0700

    Added file parsing for txt and csv files.
    Currently prints the contents of the ArrayList at the end for testing
    purposes. The file path is a local path and will not work on another
    machine. To test, create a simple txt or csv file with double values and
    update the file path.

--- GradeAnalytics/src/main/java/GradeAnalytics.java
+++ GradeAnalytics/src/main/java/GradeAnalytics.java
@@ -1,10 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class GradeAnalytics {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		String fileName = "/Users/Keenan/Desktop/testData.txt";
 		
+		ArrayList<Integer> data = parseFile(fileName, 0);
+	}
+	
+	private static ArrayList<Integer> parseFile(String fileName, int fileType) {
+		//.txt file is a fileType of 0
+		//.csv file is a fileType of 1
+		//No other file types are supported
+		BufferedReader br = null;
+		String line = "";
+		
+		ArrayList<Integer> data = new ArrayList<Integer>();
+		
+		try {
+			String delim;
+			if (fileType == 0) {
+				delim = " ";
+			} else {
+				delim = ",";
+			}
+			br = new BufferedReader(new FileReader(fileName));
+			
+			while ((line = br.readLine()) != null) {
+				String[] lineRead = line.split(delim);
+				for (int i = 0; i < lineRead.length; i++) {
+					data.add(Integer.parseInt(lineRead[i]));
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return data;
 	}
 	
 }
