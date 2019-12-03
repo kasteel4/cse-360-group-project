f1cd7f5db3094d83d3c422ca9856cbaeab86205c
kasteel4 <kasteel4@asu.edu>
2019-12-03 16:00:10 -0700

    Added InvalidFileTypeException for file parsing.

--- GradeAnalytics/src/main/java/GradeAnalytics.java
+++ GradeAnalytics/src/main/java/GradeAnalytics.java
@@ -10,31 +10,43 @@ public class GradeAnalytics {
 		
 		String fileName = "/Users/Keenan/Desktop/testData.txt";
 		
-		ArrayList<Integer> data = parseFile(fileName, 0);
+		ArrayList<Double> data = new ArrayList<Double>();
+		try {
+			data = parseFile(fileName, 0);
+		} catch (InvalidFileTypeException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		for (double i : data) {
+			System.out.println(i);
+		}
 	}
 	
-	private static ArrayList<Integer> parseFile(String fileName, int fileType) {
+	private static ArrayList<Double> parseFile(String fileName, int fileType)
+	throws InvalidFileTypeException {
 		//.txt file is a fileType of 0
 		//.csv file is a fileType of 1
 		//No other file types are supported
 		BufferedReader br = null;
 		String line = "";
 		
-		ArrayList<Integer> data = new ArrayList<Integer>();
+		ArrayList<Double> data = new ArrayList<Double>();
 		
 		try {
 			String delim;
 			if (fileType == 0) {
 				delim = " ";
-			} else {
+			} else if (fileType == 1) {
 				delim = ",";
+			} else {
+				throw new InvalidFileTypeException("Not a supported file type.");
 			}
 			br = new BufferedReader(new FileReader(fileName));
 			
 			while ((line = br.readLine()) != null) {
 				String[] lineRead = line.split(delim);
 				for (int i = 0; i < lineRead.length; i++) {
-					data.add(Integer.parseInt(lineRead[i]));
+					data.add(Double.parseDouble(lineRead[i]));
 				}
 			}
 		} catch (FileNotFoundException e) {
