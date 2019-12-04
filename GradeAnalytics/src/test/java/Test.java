95057cd1e52a09e24761ba1b409ca408335c30bc
kasteel4 <kasteel4@asu.edu>
2019-12-04 16:22:08 -0700

    Added unit test for getMode method

--- GradeAnalytics/src/test/java/Test.java
+++ GradeAnalytics/src/test/java/Test.java
@@ -18,5 +18,22 @@ class Test {
 		double mean = ga.getMean(data);
 		assertTrue(mean == 3.0);
 	}
+	
+	@org.junit.jupiter.api.Test
+	void getModeTest() {
+		ArrayList<Double> data = new ArrayList<Double>();
+		GradeAnalytics ga = new GradeAnalytics();
+		data.add(3.0);
+		data.add(4.0);
+		data.add(2.0);
+		data.add(2.0);
+		ArrayList<Double> mode = ga.getMode(data);
+		assertTrue(mode.contains(2.0));
+		data.add(3.0);
+		mode = ga.getMode(data);
+		assertTrue(mode.contains(2.0));
+		assertTrue(mode.contains(3.0));
+		
+	}
 
 }
