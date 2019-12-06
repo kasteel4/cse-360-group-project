9348040da14de62d8ae56c955d7785bc3eda6245
kasteel4 <kasteel4@asu.edu>
2019-12-06 15:07:48 -0700

    Unit test for analyzeData method

--- GradeAnalytics/src/test/java/Test.java
+++ GradeAnalytics/src/test/java/Test.java
@@ -20,7 +20,6 @@ class Test {
 	
 	@org.junit.jupiter.api.Test
 	void getModeTest() {
-		ArrayList<Double> data = new ArrayList<Double>();
 		GradeAnalytics ga = new GradeAnalytics();
 		ga.addData(3.0);
 		ga.addData(4.0);
@@ -35,8 +34,21 @@ class Test {
 	}
 	
 	@org.junit.jupiter.api.Test
-	void setBoundariesTest() {
+	void analyzeDataTest() {
 		GradeAnalytics ga = new GradeAnalytics();
+		ga.addData(2.0);
+		ga.addData(3.0);
+		ga.addData(3.0);
+		ga.addData(4.0);
+		ga.addData(5.0);
+		ga.addData(6.0);
+		ArrayList<Double> results = ga.analyzeData();
+		assertTrue(results.get(0) == 2.0);
+		assertTrue(results.get(1) == 6.0);
+		assertTrue(results.get(2) == 23.0/6);
+		assertTrue(results.get(3) == 3.5);
+		assertTrue(results.get(4) == 3.0);
+		assertTrue(results.get(5) == 3.0);
 	}
 
 }
